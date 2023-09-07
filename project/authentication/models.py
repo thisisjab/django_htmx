128bb712faa3d1658d2f851b292d414cac6ec9a9
thisisjab <heythisisjab@gmail.com>
2023-09-07 15:28:43 +0330

    feat(authentication): create `User` model

--- project/authentication/models.py
+++ project/authentication/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from project.authentication.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User model that is used in this project. Email is the username field."""
+
+    email = models.EmailField(_("Email Address"), unique=True)
+    email_verified = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_deleted = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -109,6 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication
+AUTH_USER_MODEL = "authentication.User"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/

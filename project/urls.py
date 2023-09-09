59338eaea15fbdd4242337336ba80625386fa77d
thisisjab <heythisisjab@gmail.com>
2023-09-09 15:12:19 +0330

    feat: register urls for `authentication` app

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from project.authentication.urls import urlpatterns as authentication_urls
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("account/", include(authentication_urls)),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

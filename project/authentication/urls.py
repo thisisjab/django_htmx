5300cbbbc4e1cdf995d79858289f9c143980c062
thisisjab <heythisisjab@gmail.com>
2023-09-21 18:39:35 +0330

    feat: add signup page (view, url, template, related settings)

--- project/authentication/urls.py
+++ project/authentication/urls.py
@@ -5,4 +5,5 @@ from project.authentication import views
 app_name = "auth"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
 ]
--- project/authentication/views.py
+++ project/authentication/views.py
@@ -1,5 +1,15 @@
 from django.contrib.auth.views import LoginView as BaseLoginView
+from django.views import generic
+from django.urls import reverse_lazy
+
+from project.authentication import forms as auth_forms
 
 
 class LoginView(BaseLoginView):
     template_name = "accounts/login.html"
+
+
+class SignUpView(generic.CreateView):
+    form_class = auth_forms.UserCreationForm
+    success_url = reverse_lazy("auth:login")
+    template_name = "accounts/signup.html"

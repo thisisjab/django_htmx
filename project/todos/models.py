0fc63020494b402bea8b18ce9d632b20d681b695
thisisjab <heythisisjab@gmail.com>
2023-09-22 12:01:53 +0330

    feat(todos): remove description field and add state to `Todo` model

--- project/todos/models.py
+++ project/todos/models.py
@@ -4,17 +4,38 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Todo(models.Model):
+    class State(models.TextChoices):
+        DONE = "D", "Done"
+        FINISHED = "F", "Finished"
+        IN_PROGRESS = "I", "In Progress"
+        PENDING = (
+            "P",
+            "Pending",
+        )
+        OVERDUE = "O", "Overdue"
+
     title = models.CharField(
-        max_length=255, verbose_name=_("Title"), blank=False, null=False
+        max_length=255,
+        verbose_name=_("Title"),
+        blank=False,
+        null=False,
     )
-    description = models.TextField(verbose_name=_("Description"), blank=True, null=True)
+    state = models.CharField(
+        _("State"),
+        max_length=2,
+        choices=State.choices,
+        default=State.PENDING,
+    )
     due = models.DateTimeField(blank=True, null=True, verbose_name=_("Due"))
     modification_time = models.DateTimeField(
-        verbose_name=_("Modification Time"), auto_now=True
+        verbose_name=_("Modification Time"),
+        auto_now=True,
     )
     order = models.PositiveIntegerField()
     user = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, verbose_name=_("User")
+        get_user_model(),
+        on_delete=models.CASCADE,
+        verbose_name=_("User"),
     )
 
     class Meta:

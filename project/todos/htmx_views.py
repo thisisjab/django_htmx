ce410c6d58863a7f082647b48f36296e162441fe
thisisjab <heythisisjab@gmail.com>
2023-09-22 14:31:02 +0330

    feat(todos): create add-todo feature in todo list

---
+++ project/todos/htmx_views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.utils.timezone import datetime
+
+from project.todos.models import Todo
+
+
+def add_todo(request):
+    user = request.user
+    todo_title = request.POST.get("todo_title")
+    todo_due = request.POST.get("todo_due")
+    todo = Todo(
+        user=user,
+        title=todo_title,
+        due=None if todo_due == "" else datetime.fromisoformat(todo_due),
+        order=Todo.objects.filter(user=user).last().order + 1 or 1,
+    )
+
+    todo.save()
+
+    user_todos = Todo.objects.filter(user=user).order_by("order")
+
+    return render(
+        request,
+        "todos/partial/list.html",
+        context={"user_todos": user_todos},
+    )
--- project/todos/urls.py
+++ project/todos/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import include, path
 
-from project.todos import views
+from project.todos import htmx_views, views
 
 app_name = "todo"
+
+htmx_urlpatterns = [
+    path("add/", htmx_views.add_todo, name="add"),
+]
+
 urlpatterns = [
     path("list/", views.TodoListView.as_view(), name="list"),
+    path("", include(htmx_urlpatterns)),
 ]

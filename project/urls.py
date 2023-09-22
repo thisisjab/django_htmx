3856683d090f73550f4f8b8fc84d41f39a521bc8
thisisjab <heythisisjab@gmail.com>
2023-09-22 12:07:25 +0330

    feat(todos): register `todos` app urls in project urls

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 
 from project.authentication.urls import urlpatterns as authentication_urls
+from project.todos.urls import urlpatterns as todo_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include((authentication_urls, "auth"), namespace="auth")),
+    path("todo/", include((todo_urls, "todo"), namespace="todo")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
